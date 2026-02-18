b1d9bb67c45476ad9948368e1b0777f76d12dddf
kogicko240 <dmn120112@gmail.com>
2026-02-18 17:02:55 +0300

    Add project files.

---
+++ Task01project/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+
+	const int SIZE = 10;
+	int array[SIZE];
+
+	cout << "input your array elements: ";
+	for (int index = 0; index < SIZE; index++) {
+		cin >> array[index];
+	}
+
+	int positive_sum = 0;
+	int negative_sum = 0;
+
+}
